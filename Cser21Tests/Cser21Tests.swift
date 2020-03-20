c1516155c70ba38fe8df6a58a26b0709bdf5d343
Ng Hung <tienhung@cfco.com.vn>
2020-03-20 09:40:31 +0700

    Initial Commit

---
+++ Cser21Tests/Cser21Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Cser21Tests.swift
+//  Cser21Tests
+//
+//  Created by Hung-Catalina on 3/20/20.
+//  Copyright © 2020 Hung-Catalina. All rights reserved.
+//
+
+import XCTest
+@testable import Cser21
+
+class Cser21Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
